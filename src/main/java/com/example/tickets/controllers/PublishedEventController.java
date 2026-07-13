70266e4c0108900fbfe14e49557f1340b0a5cb47
MohitSinha11 <sinhamohit1122@gmail.com>
2026-07-13 13:24:52 +0530

    Added Search functionality for Published events End point

--- src/main/java/com/example/tickets/controllers/PublishedEventController.java
+++ src/main/java/com/example/tickets/controllers/PublishedEventController.java
@@ -2,6 +2,7 @@ package com.example.tickets.controllers;
 
 import com.example.tickets.domain.dtos.ListEventResponseDto;
 import com.example.tickets.domain.dtos.ListPublishedEventResponseDto;
+import com.example.tickets.domain.entities.Event;
 import com.example.tickets.mappers.EventMapper;
 import com.example.tickets.services.EventService;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,8 +23,18 @@ public class PublishedEventController {
     private final EventMapper eventMapper;
 
     @GetMapping
-    public ResponseEntity<Page<ListPublishedEventResponseDto>>  listPublishedEvents(Pageable pageable) {
-        return ResponseEntity.ok(eventService.listPublishedEvents(pageable)
-                .map(eventMapper::toListPublishedEventResponseDto));
+    public ResponseEntity<Page<ListPublishedEventResponseDto>>  listPublishedEvents(
+            @RequestParam(required = false) String q,
+            Pageable pageable) {
+
+        Page<Event> events;
+
+        if(q != null && !q.trim().isEmpty()){
+            events = eventService.searchPublishedEvents(q, pageable);
+        }else{
+            events = eventService.listPublishedEvents(pageable);
+        }
+
+        return ResponseEntity.ok(events.map(eventMapper::toListPublishedEventResponseDto));
     }
 }
