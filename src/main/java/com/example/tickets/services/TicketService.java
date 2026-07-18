e79dc1548c7da878926a7ebb1f808798044f0519
MohitSinha11 <sinhamohit1122@gmail.com>
2026-07-18 10:14:14 +0530

    Added Get ticket and  Get QR Code Endpoints

--- src/main/java/com/example/tickets/services/TicketService.java
+++ src/main/java/com/example/tickets/services/TicketService.java
@@ -4,8 +4,10 @@ import com.example.tickets.domain.entities.Ticket;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TicketService {
     Page<Ticket> listTicketsForPurchaser(UUID purchaserId, Pageable pageable);
+    Optional<Ticket> getTicketForUser(UUID userId, UUID ticketId);
 }
